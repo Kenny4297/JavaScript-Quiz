9abe6fad48df7af4d573071ec4e9ed428268e6b1
Kenny Cordero <GeckoB4I@gmail.com>
2022-12-29 12:50:34 -0600

    removed unecessary class

--- assets/script.js
+++ assets/script.js
@@ -1,170 +1,167 @@
-class Quiz {
-    constructor () {}
+questions = [
+    {
+        question: "In what decade was Javascript invented?",
+        answers: ["1980", "1990", "2000", "2020"],
+        rightAnswer: "1990"
+    },
 
-    questions = [
-        {
-            question: "In what decade was Javascript invented?",
-            answers: ["1980", "1990", "2000", "2020"],
-            rightAnswer: "1990"
-        },
+    {
+        question: "What kind of loop is best to use when iterating through a string?",
+        answers: ["if...else", "for...while", "for", "do...while"],
+        rightAnswer: "for"
+    },
 
-        {
-            question: "What kind of loop is best to use when iterating through a string?",
-            answers: ["if...else", "for...while", "for", "do...while"],
-            rightAnswer: "for"
-        },
+    {
+        question: "Promises are real time representations of what?",
+        answers: ["loops", "if...then statements", "variables", "functions"],
+        rightAnswer: "if...then statements"
+    },
 
-        {
-            question: "Promises are real time representations of what?",
-            answers: ["loops", "if...then statements", "variables", "functions"],
-            rightAnswer: "if...then statements"
-        },
+    {
+        question: "Select the built in JavaScript method:",
+        answers: [".toMain()", ".withDecor()", ".toBold()",".toUppercase()"],
+        rightAnswer: ".toUppercase()"
+    },
 
-        {
-            question: "Select the built in JavaScript method:",
-            answers: [".toMain()", ".withDecor()", ".toBold()",".toUppercase()"],
-            rightAnswer: ".toUppercase()"
-        },
+    {
+        question: "JavaScript runs on what built in browser engine?",
+        answers: ["V8", "C++", "Diesel", "Tesla"],
+        rightAnswer: "V8"
+    },
+]
 
-        {
-            question: "JavaScript runs on what built in browser engine?",
-            answers: ["V8", "C++", "Diesel", "Tesla"],
-            rightAnswer: "V8"
-        },
-    ]
-
-    updateQuestion = () => {
-        // console.log(count);
-        if (count === 5 ) {
-            return this.endPage();
-        }
-        rightOrWrong.style.display = 'none';
-        scoreDisplay.innerText = `Current score: ${score}`;
-
-        title.innerText = this.questions[count].question;
-    
-        q1.innerText = this.questions[count].answers[0]
-        q2.innerText = this.questions[count].answers[1]
-        q3.innerText = this.questions[count].answers[2]
-        q4.innerText = this.questions[count].answers[3]
-    };
-
-    checkForCorrectAnswer = (event) => {
-        console.log("'Check for correct answer' function check")
-        if (event.target.textContent === this.questions[count].rightAnswer) {
-            // console.log("Correct")
-            count++
-            console.log(`The count for a correct selection: ${count}`)
-            score += 10
-            // scoreDisplay.innerText = `Current score: ${score}`;
-            // console.log(`This is the current score: ${score}`);
-            rightOrWrong.style.display = 'block';
-            rightOrWrong.style.color = "darkgreen";
-            rightOrWrong.innerText = "Correct!"
-            event.target.style.backgroundColor = "darkgreen";
-            setTimeout(() => {
-                event.target.style.backgroundColor = "purple";
-                if (count === 5) {
-                    // event.stopImmediatePropagation();
-                    // event.preventDefault();
-                    return this.endPage();
-                }
-                this.updateQuestion();
-            }, );
-        } else {
-            // console.log("wrong answer")
-            count++
-            console.log(`the count after an incorrect selection:${count}`)
-            rightOrWrong.style.display = 'block';
-            rightOrWrong.style.color = "darkred"
-            rightOrWrong.innerText = "Wrong!";
-            event.target.style.backgroundColor = "darkred";
-            gameTime -= 10;
-            setTimeout(() => {
-                event.target.style.backgroundColor = "purple";
-                if (count === 5) {
-                    // event.stopImmediatePropagation();
-                    // event.preventDefault();
-                    return this.endPage();
-                }
-                this.updateQuestion();
-            }, );
-        }
+const updateQuestion = () => {
+    // console.log(count);
+    if (count === 5 ) {
+        return endPage();
     }
+    rightOrWrong.style.display = 'none';
+    scoreDisplay.innerText = `Current score: ${score}`;
 
-    nextQuestion = () => {
-        console.log("'nextQuestion()' function check")
-        Array.from(buttonArray).forEach((button) => {
-            button.addEventListener('click', (this.checkForCorrectAnswer))
+    title.innerText = questions[count].question;
+
+    q1.innerText = questions[count].answers[0]
+    q2.innerText = questions[count].answers[1]
+    q3.innerText = questions[count].answers[2]
+    q4.innerText = questions[count].answers[3]
+};
+
+const checkForCorrectAnswer = (event) => {
+    console.log("'Check for correct answer' function check")
+    if (event.target.textContent === questions[count].rightAnswer) {
+        console.log("Correct")
+        count++
+        console.log(`The count for a correct selection: ${count}`)
+        score += 10
+        // scoreDisplay.innerText = `Current score: ${score}`;
+        // console.log(`This is the current score: ${score}`);
+        rightOrWrong.style.display = 'block';
+        rightOrWrong.style.color = "darkgreen";
+        rightOrWrong.innerText = "Correct!"
+        event.target.style.backgroundColor = "darkgreen";
+        setTimeout(() => {
+            event.target.style.backgroundColor = "purple";
+            if (count === 5) {
+                // event.stopImmediatePropagation();
+                // event.preventDefault();
+                return endPage();
+            }
+            updateQuestion();
+        }, 1000);
+    } else {
+        // console.log("wrong answer")
+        count++
+        console.log(`the count after an incorrect selection:${count}`)
+        rightOrWrong.style.display = 'block';
+        rightOrWrong.style.color = "darkred"
+        rightOrWrong.innerText = "Wrong! -10 seconds!";
+        event.target.style.backgroundColor = "darkred";
+        gameTime -= 10;
+        setTimeout(() => {
+            event.target.style.backgroundColor = "purple";
+            if (count === 5) {
+                // event.stopImmediatePropagation();
+                // event.preventDefault();
+                return endPage();
+            }
+            updateQuestion();
+        }, 1000);
+    }
+}
+
+const nextQuestion = () => {
+    console.log("'nextQuestion()' function check")
+    Array.from(buttonArray).forEach((button) => {
+        button.addEventListener('click', (checkForCorrectAnswer))
+    })
+};
+
+const endPage = () => {
+    stopTimer();
+    gameTime = 30;
+    // removeEventListener(this.checkForCorrectAnswer);
+    quizSection.style.display = "none";
+    endPageDisplay.style.display = "flex";
+
+    document.getElementById("finalScore").innerHTML = score;
+    count = 0;
+
+    goBackButton();
+
+    if (score > localStorageHighScores[0].finalScore) {
+        highScoreRecord.style.display = 'flex';
+        submitButton.addEventListener('click', (event) => {
+            endPageDisplay.style.display = "none";
+            highScoresPage.style.display = 'flex';
         })
-    };
-
-    endPage = () => {
-        this.stopTimer();
-        gameTime = 30;
-        // removeEventListener(this.checkForCorrectAnswer);
-        quizSection.style.display = "none";
-        endPageDisplay.style.display = "flex";
-
-        document.getElementById("finalScore").innerHTML = score;
-        count = 0;
-
-        this.goBackButton();
-
-        if (score > localStorageHighScores[0].finalScore) {
-            highScoreRecord.style.display = 'flex';
-            submitButton.addEventListener('click', (event) => {
-                endPageDisplay.style.display = "none";
-                highScoresPage.style.display = 'flex';
-            })
-        }
-    };
-
-    goBackPage = () => {
-        // clearInterval(tickingTimer);
-        this.stopTimer();
-        highScoresPage.style.display = 'none';
-        endPageDisplay.style.display = "none";
-        homePageSection.style.display = 'flex';
-    }
-
-    goBackButton = () => {
-        goBackButtonArray.forEach((button) => {
-            button.addEventListener('click', () => {
-                quiz.goBackPage();
-            })
-        })
-    }
-
-    stopTimer = () => {
-        clearInterval(quizTimer);
-    }
-
-    startTimer = () => {
-        quizTimer;
-    }
-
-    clearHighScores = () => {
-        console.log("clear high scores function check")
-        localStorageHighScores = [];
-        localStorage.clear();
-
-        let dummyScore = {
-            playerName: "loser",
-            finalScore: 0
-        }
-        
-        localStorageHighScores.push(dummyScore);
-        localStorageHighScores.push(dummyScore);
-        localStorageHighScores.push(dummyScore);
-
-        document.getElementById("high-scores-list1").innerText = `${localStorageHighScores[0].playerName} : ${localStorageHighScores[0].finalScore}`
-
-        document.getElementById("high-scores-list2").innerText = `${localStorageHighScores[1].playerName} : ${localStorageHighScores[1].finalScore}`
-
-        document.getElementById("high-scores-list3").innerText = `${localStorageHighScores[2].playerName} : ${localStorageHighScores[2].finalScore}`
     }
 };
+
+const goBackPage = () => {
+    // clearInterval(tickingTimer);
+    stopTimer();
+    highScoresPage.style.display = 'none';
+    endPageDisplay.style.display = "none";
+    homePageSection.style.display = 'flex';
+}
+
+const goBackButton = () => {
+    goBackButtonArray.forEach((button) => {
+        button.addEventListener('click', () => {
+            goBackPage();
+        })
+    })
+}
+
+const stopTimer = () => {
+    clearInterval(quizTimer);
+}
+
+const startTimer = () => {
+    quizTimer;
+}
+
+const clearHighScores = () => {
+    console.log("clear high scores function check")
+    localStorageHighScores = [];
+    localStorage.clear();
+
+    let dummyScore = {
+        playerName: "loser",
+        finalScore: 0
+    }
+    
+    localStorageHighScores.push(dummyScore);
+    localStorageHighScores.push(dummyScore);
+    localStorageHighScores.push(dummyScore);
+
+    document.getElementById("high-scores-list1").innerText = `${localStorageHighScores[0].playerName} : ${localStorageHighScores[0].finalScore}`
+
+    document.getElementById("high-scores-list2").innerText = `${localStorageHighScores[1].playerName} : ${localStorageHighScores[1].finalScore}`
+
+    document.getElementById("high-scores-list3").innerText = `${localStorageHighScores[2].playerName} : ${localStorageHighScores[2].finalScore}`
+}
+
 
 const tickingTimer = () => {
     if (gameTime >= 0) {
@@ -176,9 +173,8 @@ const tickingTimer = () => {
     }
 }
 
-const quiz = new Quiz();
 
-let gameTime = 30;
+let gameTime = 60;
 let quizTimer;
 const quizSection = document.getElementById("section");
 const startGameButton = document.querySelector("#start-game-button");
@@ -212,14 +208,14 @@ startGameButton.addEventListener('click', (event) => {
     homePageSection.style.display = "none"
     quizSection.style.display = 'flex';
     quizTimer = setInterval(tickingTimer, 1000);
-    quiz.updateQuestion();
-    quiz.nextQuestion();
+    updateQuestion();
+    nextQuestion();
 })
 
 highScoresButton.addEventListener('click', () => {
     homePageSection.style.display = 'none';
     highScoresPage.style.display = 'flex';
-    quiz.goBackButton();
+    goBackButton();
 })
 
 // Adding to local storage
@@ -284,7 +280,7 @@ const highScoresList = document.getElementById("high-scores-list");
 
 submitButton.addEventListener('click', addScore);
 
-document.getElementById("clear-high-scores").addEventListener('click', quiz.clearHighScores);
+document.getElementById("clear-high-scores").addEventListener('click', clearHighScores);
 
 
 
