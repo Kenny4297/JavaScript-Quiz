82ce9b075337262f9e85c683769d3c21955c77dd
Kenny Cordero <GeckoB4I@gmail.com>
2022-12-28 20:31:48 -0600

    still trying to figure out bug

--- assets/script.js
+++ assets/script.js
@@ -43,23 +43,28 @@ class Quiz {
 
         title.innerText = this.questions[count].question;
     
-        q1.innerText = this.questions[count].answers[0]
-        q2.innerText = this.questions[count].answers[1]
-        q3.innerText = this.questions[count].answers[2]
-        q4.innerText = this.questions[count].answers[3]
+        q1.innerHTML = this.questions[count].answers[0]
+        q2.innerHTML = this.questions[count].answers[1]
+        q3.innerHTML = this.questions[count].answers[2]
+        q4.innerHTML = this.questions[count].answers[3]
     };
 
     nextQuestion = () => {
         console.log(`Count count from "nextQuestion": ${count}`)
-        Array.from(buttonArray).forEach((button) => {
-            button.addEventListener('click', (event) => {
-                console.log("Event listener loop count")
-                if (event.target.textContent === this.questions[count].rightAnswer) {
-                    console.log("Correct")
+        //Array.from(buttonArray).forEach((button) => {
+            for(var i = 0; i <4; i++){
+            console.log("entering the for each")
+            buttonArray[i].addEventListener('click', (event) => {
+                // console.log("Event listener loop count")
+                console.log(event.target.textContent)
+                console.log(event.target.innerHTML)
+                if (event.target.innerHTML === this.questions[count].rightAnswer) {
+                    // console.log("Correct")
                     count++
+                    console.log(`count: ${count}`)
                     score += 10
-                    scoreDisplay.innerText = `Current score: ${score}`;
-                    console.log(`This is the current score: ${score}`);
+                    // scoreDisplay.innerText = `Current score: ${score}`;
+                    // console.log(`This is the current score: ${score}`);
                     rightOrWrong.style.display = 'block';
                     rightOrWrong.style.color = "darkgreen";
                     rightOrWrong.innerText = "Correct!"
@@ -74,8 +79,9 @@ class Quiz {
                         this.updateQuestion();
                     }, );
                 } else {
-                    console.log("wrong answer")
+                    // console.log("wrong answer")
                     count++
+                    console.log(`the count for an error:${count}`)
                     rightOrWrong.style.display = 'block';
                     rightOrWrong.style.color = "darkred"
                     rightOrWrong.innerText = "Wrong!";
@@ -91,22 +97,22 @@ class Quiz {
                         this.updateQuestion();
                     }, );
                 }
+               // if(count=== 5){
+                //    buttonArray[i].removeEventListener("click", btn)
+                //}
             })
-        })
+       // })
+        }
     };
 
     endPage = () => {
         this.stopTimer();
         quizSection.style.display = "none";
         endPageDisplay.style.display = "flex";
-        // if (gameTime <= 0) {
-        //     document.getElementById("you-lose").innerText = "You lose!"
-        //     document.getElementById("finalScore").innerHTML = score;
-        // }
-        // document.getElementById("you-lose").innerText = "You Win!"
+
 
         document.getElementById("finalScore").innerHTML = score;
-        count = 0;
+        // count = 0;
 
         if (score > Math.min(highScores)) {
             highScoreRecord.style.display = 'flex';
@@ -115,8 +121,6 @@ class Quiz {
                 highScoresPage.style.display = 'flex';
             })
         }
-
-
     }
 
     goBack = () => {
@@ -142,10 +146,66 @@ class Quiz {
     }
 };
 
+const nextQuestion = () => {
+    console.log(`Count count from "nextQuestion": ${count}`)
+    //Array.from(buttonArray).forEach((button) => {
+        for(var i = 0; i <4; i++){
+        console.log("entering the for each")
+        buttonArray[i].addEventListener('click', (event) => {
+            // console.log("Event listener loop count")
+            console.log(event.target.textContent)
+            console.log(event.target.innerHTML)
+            if (event.target.innerHTML === this.questions[count].rightAnswer) {
+                // console.log("Correct")
+                count++
+                console.log(`count: ${count}`)
+                score += 10
+                // scoreDisplay.innerText = `Current score: ${score}`;
+                // console.log(`This is the current score: ${score}`);
+                rightOrWrong.style.display = 'block';
+                rightOrWrong.style.color = "darkgreen";
+                rightOrWrong.innerText = "Correct!"
+                event.target.style.backgroundColor = "darkgreen";
+                setTimeout(() => {
+                    event.target.style.backgroundColor = "purple";
+                    if (count === 5) {
+                        // event.stopImmediatePropagation();
+                        // event.preventDefault();
+                        return this.endPage();
+                    }
+                    this.updateQuestion();
+                }, );
+            } else {
+                // console.log("wrong answer")
+                count++
+                console.log(`the count for an error:${count}`)
+                rightOrWrong.style.display = 'block';
+                rightOrWrong.style.color = "darkred"
+                rightOrWrong.innerText = "Wrong!";
+                event.target.style.backgroundColor = "darkred";
+                gameTime -= 10;
+                setTimeout(() => {
+                    event.target.style.backgroundColor = "purple";
+                    if (count === 5) {
+                        event.stopImmediatePropagation();
+                        event.preventDefault();
+                        return this.endPage();
+                    }
+                    this.updateQuestion();
+                }, );
+            }
+           // if(count=== 5){
+            //    buttonArray[i].removeEventListener("click", btn)
+            //}
+        })
+   // })
+    }
+};
+
 const quiz = new Quiz();
 
-let gameTime = 30;
-
+let gameTime = 300;
+let quizTimer;
 const quizSection = document.getElementById("section");
 const startGameButton = document.querySelector("#start-game-button");
 const homePageSection = document.querySelector("#home-page");
@@ -162,31 +222,30 @@ const addHighScorePrompt = document.getElementById("add-high-score-prompt");
 const highScoreRecord = document.getElementById("high-score-record");
 const submitButton = document.getElementById("btn");
 
-const buttonArray = document.getElementsByClassName("button");
+const buttonArray = document.querySelectorAll("buttons");
 const goBackButtonArray = document.getElementsByClassName("go-back-button");
-
+console.log(buttonArray[0])
 const timer = document.getElementById("timer");
 
-//First page 
-
-count = 0;
-score = 0;
+let count = 0;
+let score = 0;
 
 quizSection.style.display = 'none';
-
-// title.innerText = "Javascript Quiz"
-// paragraph.innerText = "This is a javascript quiz, and here are the instructions"
-
+// test test 
 startGameButton.addEventListener('click', (event) => {
     count = 0;
     score = 0;
+    console.log(`Count: ${count}`);
     homePageSection.style.display = "none"
     quizSection.style.display = 'flex';
-    quiz.startTimer();
+    quizTimer = setInterval(tickingTimer, 1000);
     quiz.updateQuestion();
-    quiz.nextQuestion();
+    nextQuestion();
+    console.log(`Count: ${count}`);
     console.log(`${gameTime}`)
-})
+});
+
+
 
 highScoresButton.addEventListener('click', () => {
     homePageSection.style.display = 'none';
@@ -194,21 +253,22 @@ highScoresButton.addEventListener('click', () => {
 });
 
 //WHY IS THIS ACTIVE IF THE BUTTON IS NOT AVAILABLE
+//If I click anywhere on any page, this will initiate...
 Array.from(goBackButtonArray).forEach((button) => {
     console.log("event listener button back check")
     button.addEventListener('click', () => {
-        // endPageDisplay.style.display = 'none';
         quiz.goBack();
     })
 })
 
 // Adding to local storage
-
 //This highScores array will reflect what is inside Local Storage
-let highScores = [];
+// let highScores = [];
+let highScores = JSON.parse(localStorage.getItem("highScores"));
 
 const addScore = (event) => {
     event.preventDefault();
+
     let playerScore = {
         playerName: document.getElementById("name").value,
         finalScore: score
@@ -216,24 +276,17 @@ const addScore = (event) => {
     highScores.push(playerScore);
     console.log(playerScore);
 
-    // localStorage.setItem("MyScores", JSON.stringify(highScores));
     localStorage.setItem(playerScore.playerName, playerScore.finalScore)
-
-    // console.log(`local storage: ${localStorage.getItem("MyScores")}`);
-    
-    // highScores = localStorage.getItem("MyScores");
 
     homePageSection.style.display = 'none';
     highScoresPage.style.display = 'flex';
 
-    // topScores.innerText = showScores;
-    // topScores.innerText = localStorage.getItem("MyScores");
     topScores.innerText = JSON.stringify(localStorage);
 }
 
 submitButton.addEventListener('click', addScore);
 
-
+//Timer section
 const tickingTimer = () => {
     if (gameTime >= 0) {
         timer.innerHTML = `:${gameTime}`;
@@ -244,7 +297,8 @@ const tickingTimer = () => {
     }
 }
 
-const quizTimer = setInterval(tickingTimer, 1000);
+// const quizTimer = setInterval(tickingTimer, 1000);
+//This has to run to assign it to the variable
 
 
 
